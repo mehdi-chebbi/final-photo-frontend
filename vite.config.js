25f5aed3d4b690e907b4c5d0d39ce9e3fcf5fae3
mehdi-chebbi <mehdi.chebbi@esprit.tn>
2025-10-17 10:14:14 +0100

    Push latest version, overwrite old repossss

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'phototheque.oss',
+      'localhost',
+      '.oss'
+    ],
+    host: true,
+  },
+  preview: {
+    allowedHosts: [
+      'phototheque.oss',
+      'localhost',
+      '.oss'
+    ],
+    host: true,
+  }
 })
